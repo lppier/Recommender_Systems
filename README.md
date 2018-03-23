ce3d2ee47f1e273e23477562a0944af8e220eca8
Pier <madstrum@gmail.com>
2018-03-23 10:59:47 +0800

    Remove python notebook

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Recommender_Systems
+# Exploration of Recommender Systems Using Surprise Python Package
+
+**User and Item based collaborative filtering - collaborative_filtering_exploration.py**
+
+This code demonstrates how collaborative filtering for user-based and item-based methods can be done
+in Python. 
+
+Advantages of item-based filtering over user-based filtering : 
+
+1) Scales Better : User-based filtering does not scale well as user likes/interests may change frequently. Hence, 
+the recommendation needs to be re-trained frequently. 
+
+2) Computationally Cheaper : In many cases, there are way more users than items. It makes sense to use item-based
+filtering in this case. 
+
+A famous example of item-based filtering is Amazon's recommendation engine. 
+
+https://www.cs.umd.edu/~samir/498/Amazon-Recommendations.pdf
---
+++ collaborative_filtering_exploration.py
@@ -0,0 +1,50 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from surprise import KNNWithMeans
+from surprise import Dataset
+from surprise import accuracy
+from surprise.model_selection import train_test_split
+
+# Load the movielens-100k dataset  UserID::MovieID::Rating::Timestamp
+data = Dataset.load_builtin('ml-100k')
+trainset, testset = train_test_split(data, test_size=.15)
+
+# Use user_based true/false to switch between user-based or item-based collaborative filtering
+algo = KNNWithMeans(k=50, sim_options={'user_based': True})
+algo.fit(trainset)
+
+# we can now query for specific predicions
+uid = str(196)  # raw user id
+iid = str(302)  # raw item id
+
+# get a prediction for specific users and items.
+pred = algo.predict(uid, iid, r_ui=4, verbose=True)
+
+# run the trained model against the testset
+test_pred = algo.test(testset)
+
+# get RMSE
+print("User-based Model : Test Set")
+accuracy.rmse(test_pred, verbose=True)
+
+# if you wanted to evaluate on the trainset
+print("User-based Model : Training Set")
+train_pred = algo.test(trainset.build_testset())
+accuracy.rmse(train_pred)
+
+# Use user_based true/false to switch between user-based or item-based collaborative filtering
+algo = KNNWithMeans(k=50, sim_options={'user_based': False})
+algo.fit(trainset)
+
+# run the trained model against the testset
+test_pred = algo.test(testset)
+
+# get RMSE
+print("Item-based Model : Test Set")
+accuracy.rmse(test_pred, verbose=True)
+
+# if you wanted to evaluate on the trainset
+print("Item-based Model : Training Set")
+train_pred = algo.test(trainset.build_testset())
+accuracy.rmse(train_pred)
